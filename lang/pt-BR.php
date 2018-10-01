944cb41297ed928489002a936fc9819ed4820fc0
Mauricio Eugênio <mauricio@digitalbluemarketing.com.br>
2018-10-01 15:57:17 -0300

    new language for this plugin. pt-BR - #vaiBrasiliam

---
+++ lang/pt-BR.php
@@ -0,0 +1,89 @@
+<?php
+
+$imagebrowser1 = "Navegador de imagem para CKEditor";
+$imagebrowser2 = "Total:";
+$imagebrowser3 = "Imagens";
+$imagebrowser4 = "Arraste seus arquivos aqui";
+
+$uploadpanel1 = "Por favor selecione um arquivo:";
+$uploadpanel2 = "A imagem vai ser carregada para:";
+$uploadpanel3 = "As definições de upload podem ser alteradas no painel de configurações.";
+
+$panelsettings1 = "Upload path:";
+$panelsettings2 = "Por favor escolha uma pasta que exista:";
+$panelsettings3 = "Path histórico:";
+$panelsettings4 = "Configurações:";
+$panelsettings5 = "Esconder extensão de arquivo";
+$panelsettings6 = "Mostrar  extensão de arquivo";
+$panelsettings7 = "Senha:";
+$panelsettings8 = "Sair";
+$panelsettings9 = "Desativar senha";
+$panelsettings10 = "Você curtiu o plugin?";
+$panelsettings11 = "Pague um café para nós!";
+$panelsettings12 = "Suporte:";
+$panelsettings13 = "Plugin FAQ";
+$panelsettings14 = "Reporte um bug";
+$panelsettings15 = "Versão:";
+$panelsettings16 = "Creditos:";
+$panelsettings17 = "Feito com amor por Moritz Maleck";
+$panelsettings18 = "Icones:";
+$panelsettings19 = "Pacote de icones por Icons8";
+$panelsettings20 = "Alterar linguagem";
+$panelsettings21 = "Esconder seção de notícias";
+$panelsettings22 = "Mostrar seção de notícias";
+
+$newssection1 = "Você consegue desabilitar a seção de noticias nas configurações.";
+
+$buttons1 = "Ver";
+$buttons2 = "Baixar";
+$buttons3 = "Usar";
+$buttons4 = "Deletar";
+$buttons5 = "Cancelar";
+$buttons6 = "Salvar";
+$buttons7 = "Upload";
+
+$alerts1 = "Obrigado por escolher Image Uploader and Browser for CKEditor!";
+$alerts2 = "Para usar este plugin você deve setar <b>CHMOD permissão para escrever (0777)</b> para a <i>imageuploader</i> pasta no seu servidor.";
+$alerts3 = "Como mudar permissão de arquivos usando FileZilla (link externo)";
+$alerts4 = "Confira em <a href='http://imageuploaderforckeditor.altervista.org/' target='_blank'>Documentation</a> ou em <a href='http://imageuploaderforckeditor.altervista.org/support/' target='_blank'>Plugin FAQ</a> para mais informações.";
+$alerts5 = "Para usar o plugin você precisa ativar <b>JavaScript</b> no seu Navegador.";
+$alerts6 = "Como ativar JavaScript no seu Navegador (link externo)";
+$alerts7 = "Uma nova versão do Uploader and Browser para CKEditor está disponível.";
+$alerts8 = "Baixe agora!";
+$alerts9 = "A pasta";
+$alerts10 = "isto não pode ser encontrado.";
+$alerts11 = "criar uma pasta";
+
+$dltimageerrors1 = "Algum erro ocorreu.";
+$dltimageerrors2 = "Você só consegue deletar imagens. Por favor tente depois, ou delete outra imagem.";
+$dltimageerrors3 = "O arquivo que você tentou deletar não está selecionado na pasta de upload.";
+$dltimageerrors4 = "Você não consegue deletar o sistema de arquivos. Por favor tente depois ou escolha outra imagem.";
+$dltimageerrors5 = "O arquivo selecionado não pode ser deletado. Por favor tente novamente ou escolha outra imagem. Nota: Não se esqueça de configurar a permissão gravável CHMOD (0777) para a pasta imageuploader no seu servidor.";
+$dltimageerrors6 = "O arquivo que você quer deletar não existe. Por favor tente depois ou escolha outra imagem.";
+
+$uploadimgerrors1 = "O arquivo não é uma imagem.";
+$uploadimgerrors2 = "Desculpe, o arquivo não existe.";
+$uploadimgerrors3 = "Desculpe, seu arquivo é muito grande.";
+$uploadimgerrors4 = "Desculpe, apenas JPG, JPEG, PNG & GIF arquivo são permitidos.";
+$uploadimgerrors5 = "Desculpe, seu arquivo não foi carregado. Não se esqueça de configurar a permissão gravável CHMOD (0777) para a pasta imageuploader no seu servidor.";
+$uploadimgerrors6 = "Desculpe, ocorreu um erro ao enviar o seu arquivo -";
+$uploadimgerrors7 = "- Não se esqueça de configurar a permissão gravável CHMOD (0777) para a pasta imageuploader no seu servidor.";
+
+$loginerrors1 = "Usuario não encontrado, senha errada ou usuário!";
+
+$configerrors1 = "Por favor, use as configurações do plugin para alterar a visibilidade ou tente novamente.";
+$configerrors2 = "Por favor, use o navegador de imagens para alterar o estilo do arquivo ou tente novamente.";
+
+$loginsite1 = "Bem-vindo!";
+$loginsite2 = "Por favor faça login.";
+$loginsite3 = "Usuário";
+$loginsite4 = "Senha";
+$loginsite5 = "Entrar";
+
+$createaccount1 = "Por favor, crie uma nova conta (local) para impedir que outras pessoas vejam e gerenciem suas imagens.";
+$createaccount2 = "Como posso desativar a proteção por senha? (link externo).";
+$createaccount3 = "A pasta padrão de upload é <b>ckeditor/plugins/imageuploader/uploads</b>. Você pode mudar isso no painel de configurações.";
+
+$langpanel1 = "Por favor selecione uma liguagem:";
+$langpanel2 = "Atualmente selecionado: ";
+$langpanel3 = "Fechar";
